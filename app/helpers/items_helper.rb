f28de259aa8695e94e09c0919b4f3dcca4528c23
katherine-boost <katherine@boost.co.nz>
2018-09-13 09:12:53 +1200

    Tweak: pull out category name into helper function
    And per Tim's feedback, single quotes are usual for string literals.

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,9 +1,13 @@
 module ItemsHelper
-  def item_class(item)
+  def list_item_class(item)
     if item.checked
       'item-data checked'
     else
       'item-data'
     end
+  end
+
+  def category_name(item)
+    item.category ? item.category.name : 'unknown'
   end
 end
