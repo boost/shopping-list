9066a7c9b0f07594dd019e84ae51b23ee08a6cf0
katherine-boost <katherine@boost.co.nz>
2018-09-13 17:31:06 +1200

    Indicate category of a list item via an emoji

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -8,4 +8,37 @@ module ItemsHelper
   def category_name(item)
     item.category ? item.category.name : 'unknown'
   end
+
+  def category_emoji(item)
+    emojis =
+      {
+        'Fruit and Veg' => '🍏',
+        'Butchery' => '🥓',
+        'Dairy & Eggs' => '🥚',
+        'Wine' => '🍷',
+        'Beer and Cider' => '🍺',
+        'Bakery' => '🍞',
+        'Baking Supplies' => '🍞',
+        'Sanitary' => '♀️',
+        'Breakfast Cereals' => '🥣',
+        'Canned Foods' => '🥫',
+        'Cleaning Products' => '✨',
+        'Cold Drinks' => '🥤',
+        'Condiments' => '🥫',
+        'Confectionary' => '🍬',
+        'Deli' => '🥗',
+        'Desserts' => '🍰',
+        'Frozen Foods' => '❄️',
+        'Health' => '⚕️',
+        'Hot Drinks' => '☕',
+        'Household' => '🏠',
+        'Jams, Honey and Spreads' => '🍯',
+        'Laundry' => '👚',
+        'Snack Foods' => '🥨',
+        'Stationary' => '📝',
+        'Misc' => '❔'
+      }
+
+    item.category ? emojis[item.category.name] : '❔'
+  end
 end
