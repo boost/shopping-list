ecd3aa069e76a18da5483f63f06b8e3ef60b9db6
katherine-boost <katherine@boost.co.nz>
2018-10-05 15:13:42 +1300

    Testing: name changes in specs (List → ShoppingList)

---
+++ spec/factories/shopping_lists.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :shopping_list do
+    name { 'My awesome shopping list' }
+  end
+end
--- spec/features/helpers.rb
+++ spec/features/helpers.rb
@@ -1,8 +1,8 @@
 module FeatureHelpers
-  def update_list_name(name)
+  def update_shopping_list_name(name)
     find('.js-edit-list-title').click
     title_form = find('.js-list-title-form')
-    title_form.fill_in 'list[name]', with: name
+    title_form.fill_in 'shopping_list[name]', with: name
     find('.js-submit-form').click
   end
 end
