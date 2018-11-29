f1eb05cc339109c631ee7541f99b4f57c3aef924
Theo Paul <theo@boost.co.nz>
2018-11-30 11:54:25 +1300

    Add :theme attribute to WelcomMessage model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_02_011107) do
+ActiveRecord::Schema.define(version: 2018_11_29_222024) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2018_11_02_011107) do
     t.string "colour"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "theme"
   end
 
   add_foreign_key "items", "categories"
